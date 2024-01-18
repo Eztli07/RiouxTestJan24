adba1769cada4f3dcf001f04b77fcdca5fda8a09
1009464 <1009464@g.mvusd.net>
2024-01-17 19:49:44 -0800

    Initial commit by Rioux Jan 17, 2024

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Jasmine!");
+        System.out.println("I hope you are having a good day!");
+    }
+}
